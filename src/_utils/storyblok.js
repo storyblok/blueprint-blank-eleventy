30bca5389e2b6e8de774e7c267ef42c48d39cea4
Jeremias Menichelli <jeremiasmenichelli@Jeremiass-MacBook-Pro-2.local>
2025-05-13 18:02:41 +0200

    Add README

--- src/_utils/storyblok.js
+++ src/_utils/storyblok.js
@@ -2,7 +2,7 @@ import { apiPlugin, storyblokInit } from '@storyblok/js';
 import 'dotenv/config';
 
 const { storyblokApi } = storyblokInit({
-  accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
+  accessToken: process.env.STORYBLOK_DELIVERY_API_TOKEN,
   apiOptions: {
     region: 'eu', // Choose the correct region from your Space.
   },
