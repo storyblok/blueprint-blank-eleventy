74b9d611c5eac783ceb485879b956bc75718f4bb
Jeremias Menichelli <jeremiasmenichelli@Jeremiass-MacBook-Pro-2.local>
2025-05-14 01:15:44 +0200

    Fix ignore rule and build error

--- src/_data/pages.js
+++ src/_data/pages.js
@@ -2,13 +2,11 @@ import storyblok from '../_utils/storyblok.js';
 import Feature from '../_components/feature.js'
 import Teaser from '../_components/teaser.js'
 import Grid from '../_components/grid.js'
-import FeaturedArticles from '../_components/featured_articles.js'
 
 export default async function pages() {
   const response = await storyblok.getAll('cdn/stories', {
     version: 'draft', // or "published"
     content_type: 'page',
-    resolve_relations: "featured-articles.featured_articles"
   });
   
   return response.map(story => {
@@ -23,8 +21,6 @@ export default async function pages() {
             return Teaser(blok);
           case 'grid':
             return Grid(blok);
-          case 'featured-articles':
-            return FeaturedArticles(blok);
           default:
             throw new Error(`Could not resolve ${blok.component} block.`);
         }
